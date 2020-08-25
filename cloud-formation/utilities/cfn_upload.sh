bfe5c143d034dd9a4310fb572af892eb4045a54a
EC2 Default User <ec2-user@ip-172-31-17-222.eu-west-1.compute.internal>
2020-08-25 20:04:50 +0000

    More cfn templates

--- cloud-formation/utilities/cfn_upload.sh
+++ cloud-formation/utilities/cfn_upload.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #Assuming there is an env variable with the name of the bucket saved
 BUCKET=$CFN_BUCKET
-echo $BUCKET
 BUCKET_PREFIX=$(pwd | sed  -r "s/.*aws\/cloud-formation\///")
 aws s3 cp $1 s3://$BUCKET/$BUCKET_PREFIX/$1 ${@:2}
